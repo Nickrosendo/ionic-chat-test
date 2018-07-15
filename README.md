a2261bfb949e499b8dc8b7c14bb77d5977593bc2
Nicolas Oliveira Rosendo <nicolasrosendo@hotmail.com>
2018-07-15 17:31:19 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+"# ionic-chat-test" 
+
+"# Aplicação Ionic 3 para verificar um bug que ocorre com o teclado do IOS no Chat do Octadesk"
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from '@angular/core'
+import { NavController } from 'ionic-angular'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  constructor(public navCtrl: NavController) {}
 
-  constructor(public navCtrl: NavController) {
-
+  _window(): any {
+    return window
   }
 
+  openChat() {
+    if (this._window().octadesk) {
+      this._window().octadesk.chat.toggle()
+    }
+  }
 }
